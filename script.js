6d3f0033b3b0052e0e131b01aa4c0c2b9f3aebff
PlusLibrary <cpluslib@gmail.com>
2022-01-11 03:25:40 +0900

    init commit

---
+++ script.js
@@ -0,0 +1,41 @@
+(() => {
+    const dataLink = "https://gist.githubusercontent.com/PlusLibrary/5a3da3bb6d11433bb1db740d1511645e/raw/9f58343ebee31e0029e84da22668b8d6f03dd12f/llas_biglive_shop.json";
+
+    (() => {
+        fetch(dataLink)
+            .then(response => response.json())
+            .then(data => main(data));
+    })();
+
+    const $ = query => document.querySelector(query);
+    const main = raw => {
+        const data = Object.values(raw.reduce((previous, current) => {
+            previous[current.name] ??= { "name": current.name, "UR": 0, "SR": 0 };
+            previous[current.name][current.rarity]++;
+            return previous;
+        }, [])).sort((a, b) => b.UR + b.SR - a.UR - a.SR);
+        const chart = {
+            labels: data.map(value => value.name),
+            datasets: [{
+                label: "UR",
+                data: data.map(value => value.UR),
+                backgroundColor: ["#ECE"]
+            }, {
+                label: "SR",
+                data: data.map(value => value.SR),
+                backgroundColor: ["#CEE"]
+            }]
+        };
+        new Chart($("#chart"), {
+            type: "bar",
+            data: chart,
+            options: {
+                indexAxis: "y",
+                scales: {
+                    y: { stacked: true },
+                    x: { stacked: true },
+                }
+            },
+        });
+    };
+})();
